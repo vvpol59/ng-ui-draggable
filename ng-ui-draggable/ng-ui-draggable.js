280dc29dd76895eac91ba2c95ccc6b82452335b6
vvpol59 <vvpol@mail.ru>
2016-11-14 22:13:29 +0300

    ограничение перемещения контейнером

--- ng-ui-draggable/ng-ui-draggable.js
+++ ng-ui-draggable/ng-ui-draggable.js
@@ -9,41 +9,101 @@
         return {
             restrict: 'A',
             link: function(scope, element, attrs){
-                var startX, startY, initialMouseX, initialMouseY, rParams, handler,
+                var startX,
+                    startY,
+                    initialMouseX,
+                    initialMouseY,
+                    rParams,
+                    handler,
+                    width,
+                    height,
+                    container,
+                    containment = {},
                     params = {
-                        handlerClass: false
+                        handlerClass: false,
+                        axis: false,
+                        containment: false
                     };
+
+                /**
+                 * Обработчик перемещения
+                 * @param e
+                 * @returns {boolean}
+                 */
                 function mousemove(e){
-                    var dx = e.clientX - initialMouseX;
-                    var dy = e.clientY - initialMouseY;
+                    e.stopPropagation();
+                    e.preventDefault();
+                    var dx = e.clientX - initialMouseX,
+                        dy = e.clientY - initialMouseY,
+                        newY = startY + dy,
+                        newX = startX + dx;
+                    if (newX < 0){
+                        newX = 0;
+                    }
+                    if (newY < 0){
+                        newY = 0;
+                    }
+                    if (newX + width > containment.right){
+                        newX = containment.right - width;
+                    }
+                    if (newY + height > containment.bottom){
+                        newY = containment.bottom - height;
+                    }
                     element.css({
-                        top:  startY + dy + 'px',
-                        left: startX + dx + 'px'
+                        top:  newY + 'px',
+                        left: newX + 'px'
                     });
-     //               return false;
+                    return false;
                 }
                 /**
                  * Инициализация перетаскивания по mousedown
                  * @param e
                  */
                 function initDraggable(e){
+                    e.stopPropagation();
+                    e.preventDefault();
                     startX = element.prop('offsetLeft');
                     startY = element.prop('offsetTop');
                     initialMouseX = e.clientX;
                     initialMouseY = e.clientY;
+                    // Если задан контейнер, определяемся с разменами контейнера и элемента
+                    width = element.prop('offsetWidth');
+                    height = element.prop('offsetHeight');
+                    containment = {
+                        top: 0,  //container.prop('offsetTop'),
+                        left: 0  //container.prop('offsetLeft')
+                    };
+                    containment.right = /*containment.left +*/ container.prop('offsetWidth');
+                    containment.bottom = /*containment.top +*/ container.prop('offsetHeight');
                     $document.on('mousemove', mousemove);
                     $document.one('mouseup', function() {
                         $document.off('mousemove');
                     });
                 }
-
+                // --------- Инициализация -------------------
+                element.css({position: 'absolute'});
                 rParams = attrs.ngUiDraggable ? JSON.parse(attrs.ngUiDraggable) : {};
                 if (rParams['handler-class'] != undefined){
                     params.handlerClass = rParams['handler-class'];
                 }
+                if (rParams.containment != undefined){  // Задан контейнер
+                    if (rParams.containment[0] == '.'){
 
-                element.css({position: 'absolute'});
-                handler = params.handlerClass ? angular.element(element[0].getElementsByClassName('params.handlerClass')[0]) : element;
+                    } else if (rParams.containment[0] == '#'){
+
+                    } else if (rParams.containment == 'parent'){
+                        container = element.parent();
+                    }
+                    if (container){
+                        params.containment = rParams.containment;
+                    }
+                } else {  // Контейнером назначаем body
+                    container = angular.element(document.getElementsByTagName('body')[0]);
+                }
+                if (rParams.axis != undefined){
+                    params.axis = rParams.axis;
+                }
+                handler = params.handlerClass ? angular.element(element[0].getElementsByClassName(params.handlerClass)[0]) : element;
                 handler.on('mousedown', initDraggable);
             }
         }
